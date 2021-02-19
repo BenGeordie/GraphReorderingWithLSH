49d255a389414b35d918837164e444f12bec7957
benitogeordie <bg31@rice.edu>
2021-02-19 17:46:45 -0600

    Added graph diagnostics

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from GRL import GRL
 from RandomReorder import RandomReorder
 from EgoFacebookGraph import EgoFacebookGraph
 from EvaluateGraphOrder import evaluate
+from Diagnostics import pairwise_overlap
 
 
 def main():
     graph = EgoFacebookGraph("facebook_combined.txt").export_arr()
+    pairwise_overlap("Ego Facebook", graph)
     grl = GRL(100000, len(graph))
     rr = RandomReorder(len(graph))
     for i, vec in enumerate(graph):
